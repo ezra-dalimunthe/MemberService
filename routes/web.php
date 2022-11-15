02ac382663fa75ddbd835f480008c17551523938
halotec <ezra.dalimunthe@gmail.com>
2022-11-15 18:29:09 +0700

    init

--- routes/web.php
+++ routes/web.php
@@ -59,3 +59,14 @@ $router->group(["prefix" => "api/v1/statistic"], function () use ($router) {
     $router->get("/member-by-gender", "StatisticController@memberByGender");
     $router->get("/member-by-age", "StatisticController@memberByAge");
 });
+
+
+Route::get('/dbcheck', function () {
+    // Test database connection
+    try {
+        DB::connection()->getPdo();
+        echo "database " . DB::connection()->getDatabaseName(), " is ready\n" ;
+    } catch (\Exception $e) {
+        die("Could not connect to the database. Please check your configuration. error:" . $e );
+   }
+});
